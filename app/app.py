9cc11477f57c7b23ef40a9161f30e96e1253ff1d
devlubos <136510726+devlubos@users.noreply.github.com>
2025-01-19 17:39:35 +0100

    working links for history

--- app/app.py
+++ app/app.py
@@ -3,24 +3,35 @@ import json
 
 main_app = Blueprint('main_app', __name__)
 
-
 @main_app.route("/", methods=["GET", "POST"])
 def index():
-
-    cookie_data = request.cookies.get("recent_values")
-    if cookie_data:
-        recent_values = json.loads(cookie_data)
-    else:
-        recent_values = []
-
-    if request.method == "POST":
-        url = request.form["url"]
-        if url:
-            recent_values.append(url)
-            recent_values = recent_values[-10:]        
-    else:
-        url = "https://www.google.com"
-
+    recent_values = get_recent_values()
+    url = "https://www.google.com"
+        
     response = make_response(render_template("index.html", url_link=url, recent_values=recent_values))
     response.set_cookie("recent_values", json.dumps(recent_values))
     return response
+
+
+@main_app.route('/qrcode', methods = ["GET"])
+def qrcode():
+    recent_values = get_recent_values()
+    url = request.args.get('url')
+
+    recent_values = upload_recent_values(url, recent_values)
+    
+    response = make_response(render_template("index.html", url_link=url, recent_values=recent_values))
+    response.set_cookie("recent_values", json.dumps(recent_values))
+    return response
+
+
+def get_recent_values():
+    cookie_data = request.cookies.get("recent_values")
+    return json.loads(cookie_data) if cookie_data else []
+
+
+def upload_recent_values(url, recent_values):
+    if url:
+        recent_values.append(url)
+        return recent_values[-10:]   
+    return recent_values
